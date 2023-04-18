456abdc0a0957c892c98e27b2f07f880939061ae
broharigunda <brohari740@gmail.com>
2023-04-18 10:18:36 +0530

    update tholil

---
+++ Pages/Company/companies.js
@@ -0,0 +1,5 @@
+companies=[
+    {"C_name":"E-Con Systems", "Package":"3LPA to 8LPA", "Link":"E-con_systems.html", "Roles":"Electronics and Software", "Location":"Chennai" ,"prodOrServ":"Product Based Company"},
+    {"C_name":"Jasmine info Tech", "Package":"3.75LPA", "Link":"../../indev.html", "Roles":"Electronics and Software", "Location":"Chennai" ,"prodOrServ":"Service Based Company"},
+    {"C_name":"Cognizant", "Package":"4LPA", "Link":"../../indev.html", "Roles":"IT/Software", "Location":"Chennai, Bangalore" ,"prodOrServ":"Service Based Company"},
+]
---
+++ indev.html
@@ -0,0 +1,1 @@
+<h1>In Development</h1>
